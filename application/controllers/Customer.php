37e9cabc17497006f0f7e6e8bbdd30f1262b4e79
fian <fian.fachry.fifa@gmail.com>
2019-12-26 14:30:13 +0700

    'created_date,benni:create_date'

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -4,6 +4,51 @@ class Customer extends Controller
 	public $models = ['CustomerModel']; 
 	public function index()
 	{
-		echo 'hello Customer';
+		$model = new CustomerModel();
+		$dataProvider = $model->findAll();
+		$this->render('customer/index', [
+			'model' => $dataProvider,
+		]);
 	}
-} 
+
+	public function delete()
+	{
+		$id = $this->input->post('delete_id');
+		$model = new VendorModel($id);
+		redirect($this->controllerId.'/index');
+	}
+
+	public function create()
+	{
+		$model = new CustomerModel();
+		
+		if ($this->input->post()!= null) {
+			$model->setAttributes($this->input->post());
+			if ($model->validate()) {
+				$model->insert();
+				redirect($this->controllerId.'/index');
+			}
+		}
+		$this->render('customer/form', [
+			'model' => $model,
+		]);
+		
+	}
+	public function edit($id)
+	{
+		$model = new VendorModel($id);
+
+		if ($this->input->post()!= null) {
+			$model->setAttributes($this->input->post());
+			if ($model->validate()) {
+				$model->update();
+				redirect($this->controllerId.'/index');
+			}
+		}
+		$this->render('customer/form', [
+			'model' => $model,
+			'dropdown_list' => $dropdown_list
+		]);
+		
+	}
+}
---
+++ application/controllers/Uom.php
@@ -0,0 +1,9 @@
+<?php 
+class Uom extends Controller 
+{
+	public $models = ['UomModel']; 
+	public function index()
+	{
+		echo 'hello Uom';
+	}
+} 
