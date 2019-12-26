d1970069b83957e28e745c675d6c88ae36c21ecf
unknown <khotib@docotel.com>
2019-12-26 14:03:44 +0700

    custommer pages

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -13,8 +13,40 @@ class Customer extends Controller
 	public function create()
 	{
 		$model = new CustomerModel();
+		if ($this->input->post()!=null) {
+			$model->setAttributes($this->input->post());
+			if ($model->validate()) {
+				$model->insert();
+				redirect('customer');
+			}
+		}
 		$this->render('customer/form', [
 			'model' => $model
 		]);
+	}
+	public function edit($id)
+	{
+		$model = new CustomerModel($id);
+		if ($this->input->post()!=null) {
+			$model->setAttributes($this->input->post());
+			if ($model->validate()) {
+				$model->update();
+				redirect($this->controllerId);
+			}
+		}
+		$this->render('customer/form', [
+			'model' => $model
+		]);
+	}
+	public function delete()
+	{
+		$id = $this->input->post('delete_id');
+		$model = new CustomerModel($id);
+		if ($model->remove()) {
+			redirect($this->controllerId.'/index');
+		}
+		else{
+			redirect($this->controllerId.'/index');
+		}
 	}
 } 
